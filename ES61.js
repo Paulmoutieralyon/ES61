6f6151419895b84bfd7038e9dd1bcc3279bcb654
Paul Moutier <42945383+Paulmoutieralyon@users.noreply.github.com>
2018-09-17 13:28:54 +0200

    Update ES61.js

--- ES61.js
+++ ES61.js
@@ -1,6 +1,6 @@
         let city = "Paris"
-        let age = 29
-        const dateOfBirth = "26 février 1989"
+        let age = 45
+        const dateOfBirth = "26 Juillet 1995"
 
         const sentence = `J'habite à ${city}, j'ai ${age} ans et je suis née le ${dateOfBirth}.`
 
