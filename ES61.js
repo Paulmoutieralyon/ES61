b4c0470480f7cba22b97f194a1031bfe9a45c667
Paul Moutier <42945383+Paulmoutieralyon@users.noreply.github.com>
2018-09-17 13:27:20 +0200

    Update ES61.js

---
+++ ES61.js
@@ -0,0 +1,7 @@
+        let city = "Paris"
+        let age = 29
+        const dateOfBirth = "26 février 1989"
+
+        const sentence = `J'habite à ${city}, j'ai ${age} ans et je suis née le ${dateOfBirth}.`
+
+        console.log(sentence)
